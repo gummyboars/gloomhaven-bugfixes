e4c0e746aded0997902a631fab6e855707884c3e
Ozan Demirlioglu <59465487+fingoldfish@users.noreply.github.com>
2025-11-10 01:12:14 -0800

    also fix a bug in the sun class's level 9 card

---
+++ Divine.cs
@@ -0,0 +1,25 @@
+using System;
+
+using HarmonyLib;
+
+using ScenarioRuleLibrary;
+
+
+[HarmonyPatch(typeof(CPreventDamageActiveBonus_PreventAndApplyToActiveBonusCaster), "OnPreventDamageTriggered")]
+public class Patch_DivineIntervention
+{
+    static bool Prefix(CPreventDamageActiveBonus_PreventAndApplyToActiveBonusCaster __instance, int damagePrevented, CActor damageSource, CActor damagedActor, CAbility damagingAbility)
+    {
+        int reducedDamage = (Math.Max(0, damagePrevented - __instance.m_Ability.Strength));
+        if (__instance.m_ActiveBonus.IsActiveBonusToggledAndNotRestricted(damagedActor))
+        {
+            if (reducedDamage > 0)
+            {
+                GameState.RedirectedDamageToActor = new Tuple<CActor, int>(__instance.m_ActiveBonus.Caster, reducedDamage);
+            }
+
+            __instance.OnBehaviourTriggered();
+        }
+        return false;
+    }
+}
